7d1c8d20abc7be1acee222d61a2dfdb51cd03118
Ibra <akievkrytou47@gmail.com>
2025-11-03 21:27:46 +0300

    init repo

---
+++ parsers/base.py
@@ -0,0 +1,8 @@
+{
+  "tool": "sonarqube",
+  "summary": {"issues": 12, "high": 3, "medium": 5, "low": 4},
+  "items": [
+    {"title":"SQLInjection", "severity":"HIGH", "file":"src/a.java", "line":23, "desc":"...","link":"..."},
+    ...
+  ]
+}
